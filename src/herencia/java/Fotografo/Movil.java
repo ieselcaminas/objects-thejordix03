08a39e122539e0beec06dbc6cb43f467cc5df08e
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2025-02-21 12:29:47 +0100

    ejercicios

---
+++ src/herencia/java/Fotografo/Movil.java
@@ -0,0 +1,24 @@
+package herencia.java.Fotografo;
+
+public class Movil implements TomarFoto{
+    private String modelo;
+
+    public Movil(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public void tomarFoto(){
+        System.out.println("Hago una foto con el móvil");
+    }
+
+
+}
---
+++ src/herencia/java/Fotografo/TomarFoto.java
@@ -0,0 +1,5 @@
+package herencia.java.Fotografo;
+
+public interface TomarFoto {
+    void tomarFoto();
+}
